75e9a99e99630c56e068724a01bc2c77bb30d5ca
dummy1027 <dummy1027@naver.com>
2026-05-05 16:36:04 +0900

    되겠지

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+
 const app = express();
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// 리액트가 이 주소로 요청을 보낼 거예요
+// 서버가 잘 사나 있나 확인하는 테스트용 API
 app.get('/api/test', (req, res) => {
-  res.json({ result: "서버가 정상적으로 응답하고 있습니다!" });
+    res.json({ result: "3전 4기! 드디어 서버 연결에 성공했습니다! 🦊" });
 });
 
-const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`백엔드 서버 오픈: http://localhost:${PORT}`);
+    console.log(`🚀 서버가 http://localhost:${PORT} 에서 활기차게 돌아가고 있습니다!`);
 });
