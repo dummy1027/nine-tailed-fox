4a2249eec0355b4900783ddc67c3a18e29695aef
dummy1027 <dummy1027@naver.com>
2026-05-23 00:12:15 +0900

    이건 짜잘한거

--- src/BattleArena.jsx
+++ src/BattleArena.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BattleArena = () => {
   return (
     <div style={{ padding: '100px 20px', color: 'white', textAlign: 'center' }}>
-      <h1>⚔️ 랜덤/비공개 배틀 아레나 준비 중 ⚔️</h1>
+      <h1>⚔️ 랜덤배틀 아레나 준비 중 ⚔️</h1>
     </div>
   );
 };
